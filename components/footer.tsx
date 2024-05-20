05b460e2074ee596eaa06d0c10e4899a4fa88c28
matthewglasser <matthewsglasser@gmail.com>
2024-05-20 09:02:27 -0400

    install dependencies

---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="bg-slate-500/20 text-gray-300 font-light flex w-full text-slate p-10 border-t border-slate-700/40">
+      &copy; Matthew Glasser 2024
+    </div>
+  );
+}
