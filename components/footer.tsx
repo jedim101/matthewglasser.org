d033fc22ca5062453b14230166dc6f39417c1bd6
matthewglasser <matthewsglasser@gmail.com>
2024-05-20 09:07:08 -0400

    plan pages

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
-  return (
-    <div className="bg-slate-500/20 text-gray-300 font-light flex w-full text-slate p-10 border-t border-slate-700/40">
-      &copy; Matthew Glasser 2024
-    </div>
-  );
+	return (
+		<div className="text-slate font-sm flex w-full justify-between border-t border-slate-700/40 bg-slate-500/20 p-10 text-sm font-light text-gray-400">
+			<div />
+			&copy; 2024 Matthew Glasser. All rights reserved.
+		</div>
+	);
 }
