e39634ddc45cb2c79adf64e6572346e37579b3d8
matthewglasser <matthewsglasser@gmail.com>
2024-05-28 09:22:48 -0400

    add vortex header

--- app/page.tsx
+++ app/page.tsx
@@ -2,92 +2,112 @@
 import React from "react";
 import { HeroParallax } from "@/components/hero-parallax";
 import Footer from "@/components/footer";
+import { Vortex } from "@/components/vortex";
 
-const products = [
-	{
-		title: "Moonbeam",
-		link: "https://gomoonbeam.com",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
-	},
-	{
-		title: "Cursor",
-		link: "https://cursor.so",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/cursor.png",
-	},
-	{
-		title: "Rogue",
-		link: "https://userogue.com",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/rogue.png",
-	},
+// const images = [
+// 	{
+// 		title: "Moonbeam",
+// 		link: "https://gomoonbeam.com",
+// 		thumbnail: "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
+// 	},
+// 	{
+// 		title: "Jaybots",
+// 		link: "https://jaybots.org",
+// 		thumbnail: "/images/jaybots-org.png",
+// 	},
+// 	{
+// 		title: "Rogue",
+// 		link: "https://userogue.com",
+// 		thumbnail: "https://aceternity.com/images/products/thumbnails/new/rogue.png",
+// 	},
 
-	{
-		title: "Editorially",
-		link: "https://editorially.org",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/editorially.png",
-	},
-	{
-		title: "Editrix AI",
-		link: "https://editrix.ai",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/editrix.png",
-	},
-	{
-		title: "Pixel Perfect",
-		link: "https://app.pixelperfect.quest",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
-	},
+// 	{
+// 		title: "Editorially",
+// 		link: "https://editorially.org",
+// 		thumbnail: "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+// 	},
+// 	{
+// 		title: "Editrix AI",
+// 		link: "https://editrix.ai",
+// 		thumbnail: "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+// 	},
+// 	{
+// 		title: "DeAP Learning",
+// 		link: "https://deaplearning.com",
+// 		thumbnail: "/images/deaplearing-com.png",
+// 	},
+// 	{
+// 		title: "Algochurn",
+// 		link: "https://algochurn.com",
+// 		thumbnail: "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+// 	},
+// 	{
+// 		title: "Aceternity UI",
+// 		link: "https://ui.aceternity.com",
+// 		thumbnail: "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+// 	},
+// 	{
+// 		title: "Tailwind Master Kit",
+// 		link: "https://tailwindmasterkit.com",
+// 		thumbnail: "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+// 	},
+// 	{
+// 		title: "SmartBridge",
+// 		link: "https://smartbridgetech.com",
+// 		thumbnail: "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+// 	},
+// 	{
+// 		title: "Renderwork Studio",
+// 		link: "https://renderwork.studio",
+// 		thumbnail: "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
+// 	},
 
-	{
-		title: "Algochurn",
-		link: "https://algochurn.com",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
-	},
-	{
-		title: "Aceternity UI",
-		link: "https://ui.aceternity.com",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
-	},
-	{
-		title: "Tailwind Master Kit",
-		link: "https://tailwindmasterkit.com",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
-	},
-	{
-		title: "SmartBridge",
-		link: "https://smartbridgetech.com",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
-	},
-	{
-		title: "Renderwork Studio",
-		link: "https://renderwork.studio",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
-	},
-
-	{
-		title: "Creme Digital",
-		link: "https://cremedigital.com",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
-	},
-	{
-		title: "Golden Bells Academy",
-		link: "https://goldenbellsacademy.com",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
-	},
-	{
-		title: "Invoker Labs",
-		link: "https://invoker.lol",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/invoker.png",
-	},
-	{
-		title: "E Free Invoice",
-		link: "https://efreeinvoice.com",
-		thumbnail: "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
-	},
-];
+// 	{
+// 		title: "Creme Digital",
+// 		link: "https://cremedigital.com",
+// 		thumbnail: "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+// 	},
+// 	{
+// 		title: "Golden Bells Academy",
+// 		link: "https://goldenbellsacademy.com",
+// 		thumbnail: "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+// 	},
+// 	{
+// 		title: "Invoker Labs",
+// 		link: "https://invoker.lol",
+// 		thumbnail: "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+// 	},
+// 	{
+// 		title: "E Free Invoice",
+// 		link: "https://efreeinvoice.com",
+// 		thumbnail: "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+// 	},
+// ];
 
 export default function HeroParallaxDemo() {
 	return (
 		<>
-			<HeroParallax products={products} />
+			{/* <HeroParallax images={images} /> */}
+			<div className="mx-auto h-screen w-screen  overflow-hidden rounded-md">
+				<Vortex
+					backgroundColor="black"
+					rangeY={800}
+					particleCount={500}
+					baseHue={150}
+					baseSpeed={0}
+					className="flex h-full w-full flex-col items-center justify-center  px-2 py-4 md:px-10">
+					<h2 className="text-center text-7xl font-bold text-white">Matthew Glasser</h2>
+					<p className="mt-6 max-w-xl text-center text-xl text-gray-200">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, eveniet.
+					</p>
+					<div className="mt-6 flex flex-col items-center gap-4 sm:flex-row">
+						<button className="rounded-lg bg-blue-600 px-4 py-2 text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset] transition duration-200 hover:bg-blue-700">
+							Order now
+						</button>
+						<button className="px-4 py-2  text-white ">Watch trailer</button>
+					</div>
+				</Vortex>
+			</div>
 			<Footer />
 		</>
 	);
--- components/hero-parallax.tsx
+++ components/hero-parallax.tsx
@@ -1,158 +1,112 @@
 "use client";
 import React from "react";
-import {
-  motion,
-  useScroll,
-  useTransform,
-  useSpring,
-  MotionValue,
-} from "framer-motion";
+import { motion, useScroll, useTransform, useSpring, MotionValue } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 
 export const HeroParallax = ({
-  products,
+	images,
 }: {
-  products: {
-    title: string;
-    link: string;
-    thumbnail: string;
-  }[];
+	images: {
+		title: string;
+		link: string;
+		thumbnail: string;
+	}[];
 }) => {
-  const firstRow = products.slice(0, 5);
-  const secondRow = products.slice(5, 10);
-  const thirdRow = products.slice(10, 15);
-  const ref = React.useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"],
-  });
+	const firstRow = images.slice(0, 4);
+	const secondRow = images.slice(4, 8);
+	const thirdRow = images.slice(8, 12);
+	const ref = React.useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ["start start", "end start"],
+	});
 
-  const springConfig = { stiffness: 300, damping: 30, bounce: 100 };
+	const springConfig = { stiffness: 300, damping: 30, bounce: 100 };
 
-  const translateX = useSpring(
-    useTransform(scrollYProgress, [0, 1], [0, 1000]),
-    springConfig
-  );
-  const translateXReverse = useSpring(
-    useTransform(scrollYProgress, [0, 1], [0, -1000]),
-    springConfig
-  );
-  const rotateX = useSpring(
-    useTransform(scrollYProgress, [0, 0.2], [15, 0]),
-    springConfig
-  );
-  const opacity = useSpring(
-    useTransform(scrollYProgress, [0, 0.2], [0.2, 1]),
-    springConfig
-  );
-  const rotateZ = useSpring(
-    useTransform(scrollYProgress, [0, 0.2], [20, 0]),
-    springConfig
-  );
-  const translateY = useSpring(
-    useTransform(scrollYProgress, [0, 0.2], [-700, 500]),
-    springConfig
-  );
-  return (
-    <div
-      ref={ref}
-      className=" h-[160rem] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
-    >
-      <Header />
-      <motion.div
-        style={{
-          rotateX,
-          rotateZ,
-          translateY,
-          opacity,
-        }}
-        className=""
-      >
-        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20 mb-20">
-          {firstRow.map((product) => (
-            <ProductCard
-              product={product}
-              translate={translateX}
-              key={product.title}
-            />
-          ))}
-        </motion.div>
-        <motion.div className="flex flex-row  mb-20 space-x-20 ">
-          {secondRow.map((product) => (
-            <ProductCard
-              product={product}
-              translate={translateXReverse}
-              key={product.title}
-            />
-          ))}
-        </motion.div>
-        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20">
-          {thirdRow.map((product) => (
-            <ProductCard
-              product={product}
-              translate={translateX}
-              key={product.title}
-            />
-          ))}
-        </motion.div>
-      </motion.div>
-    </div>
-  );
+	const translateX = useSpring(useTransform(scrollYProgress, [0, 1], [0, 1000]), springConfig);
+	const translateXReverse = useSpring(useTransform(scrollYProgress, [0, 1], [0, -1000]), springConfig);
+	const rotateX = useSpring(useTransform(scrollYProgress, [0, 0.2], [15, 0]), springConfig);
+	const opacity = useSpring(useTransform(scrollYProgress, [0, 0.2], [0.2, 1]), springConfig);
+	const rotateZ = useSpring(useTransform(scrollYProgress, [0, 0.2], [20, 0]), springConfig);
+	const translateY = useSpring(useTransform(scrollYProgress, [0, 0.2], [-700, 500]), springConfig);
+	return (
+		<div
+			ref={ref}
+			className=" relative flex h-[160rem]  flex-col self-auto overflow-hidden py-40 antialiased [perspective:1000px] [transform-style:preserve-3d]">
+			<Header />
+			<motion.div
+				style={{
+					rotateX,
+					rotateZ,
+					translateY,
+					opacity,
+				}}
+				className="">
+				<motion.div className="mb-20 flex flex-row-reverse space-x-20 space-x-reverse">
+					{firstRow.map((product) => (
+						<ProductCard product={product} translate={translateX} key={product.title} />
+					))}
+				</motion.div>
+				<motion.div className="mb-20 flex  flex-row space-x-20 ">
+					{secondRow.map((product) => (
+						<ProductCard product={product} translate={translateXReverse} key={product.title} />
+					))}
+				</motion.div>
+				<motion.div className="flex flex-row-reverse space-x-20 space-x-reverse">
+					{thirdRow.map((product) => (
+						<ProductCard product={product} translate={translateX} key={product.title} />
+					))}
+				</motion.div>
+			</motion.div>
+		</div>
+	);
 };
 
 export const Header = () => {
-  return (
-    <div className="max-w-5xl relative mx-auto py-20 md:py-40 px-4 w-full z-50 left-0 top-0">
-      <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        Matthew Glasser
-      </h1>
-      <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ipsa
-        quos labore cum ullam nam inventore impedit. Praesentium, quibusdam vel?
-      </p>
-    </div>
-  );
+	return (
+		<div className="relative left-0 top-0 z-50 mx-auto w-full max-w-5xl px-4 py-20 md:py-40">
+			<h1 className="text-2xl font-bold md:text-7xl dark:text-white">Matthew Glasser</h1>
+			<p className="mt-8 max-w-2xl text-base md:text-xl dark:text-neutral-200">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ipsa quos labore cum ullam nam inventore
+				impedit. Praesentium, quibusdam vel?
+			</p>
+		</div>
+	);
 };
 
 export const ProductCard = ({
-  product,
-  translate,
+	product,
+	translate,
 }: {
-  product: {
-    title: string;
-    link: string;
-    thumbnail: string;
-  };
-  translate: MotionValue<number>;
+	product: {
+		title: string;
+		link: string;
+		thumbnail: string;
+	};
+	translate: MotionValue<number>;
 }) => {
-  return (
-    <motion.div
-      style={{
-        x: translate,
-      }}
-      whileHover={{
-        y: -20,
-      }}
-      key={product.title}
-      className="group/product h-96 w-[30rem] relative flex-shrink-0"
-    >
-      <Link
-        href={product.link}
-        className="block group-hover/product:shadow-2xl "
-      >
-        <Image
-          src={product.thumbnail}
-          height="600"
-          width="600"
-          className="object-cover object-left-top absolute h-full w-full inset-0"
-          alt={product.title}
-        />
-      </Link>
-      <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none"></div>
-      <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white">
-        {product.title}
-      </h2>
-    </motion.div>
-  );
+	return (
+		<motion.div
+			style={{
+				x: translate,
+			}}
+			whileHover={{
+				y: -20,
+			}}
+			key={product.title}
+			className="group/product relative aspect-video h-96 flex-shrink-0">
+			<Link href={product.link} className="block group-hover/product:shadow-2xl ">
+				<Image
+					src={product.thumbnail}
+					height="600"
+					width="900"
+					className="absolute inset-0 h-full w-full object-cover object-left-top"
+					alt={product.title}
+				/>
+			</Link>
+			<div className="pointer-events-none absolute inset-0 h-full w-full bg-black opacity-0 group-hover/product:opacity-80"></div>
+			<h2 className="absolute bottom-4 left-4 text-white opacity-0 group-hover/product:opacity-100">{product.title}</h2>
+		</motion.div>
+	);
 };
