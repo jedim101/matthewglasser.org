3bb24f7a959916b5e11f66c23238260d36419cc9
matthewglasser <matthewsglasser@gmail.com>
2024-05-29 16:17:43 -0400

    home page buttons

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { HeroParallax } from "@/components/hero-parallax";
 import Footer from "@/components/footer";
 import { Vortex } from "@/components/vortex";
+import Link from "next/link";
 
 // const images = [
 // 	{
@@ -91,19 +92,27 @@ export default function HeroParallaxDemo() {
 			<div className="mx-auto h-screen w-screen overflow-hidden rounded-md bg-gray-950">
 				<Vortex
 					rangeY={800}
-					particleCount={500}
+					particleCount={300}
 					baseHue={150}
 					baseSpeed={0}
 					className="flex h-full w-full flex-col items-center justify-center  px-2 py-4 md:px-10">
 					<h2 className="text-center text-7xl font-bold text-white">Matthew Glasser</h2>
-					<p className="mt-6 max-w-xl text-center text-xl text-gray-200">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, eveniet.
-					</p>
+					<p className="mt-6 max-w-xl text-center text-2xl text-gray-200">Web Developer</p>
 					<div className="mt-6 flex flex-col items-center gap-4 sm:flex-row">
-						<button className="rounded-lg bg-blue-600 px-4 py-2 text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset] transition duration-200 hover:bg-blue-700">
-							Order now
-						</button>
-						<button className="px-4 py-2  text-white ">Watch trailer</button>
+						<Link
+							href="/projects"
+							className="relative rounded-full border border-slate-800 bg-slate-900 px-8 py-2 text-sm text-white transition duration-300 hover:border-slate-700 hover:bg-slate-800">
+							<div className="absolute inset-x-0 -top-px mx-auto h-px w-1/2 bg-gradient-to-r  from-transparent via-indigo-500 to-transparent shadow-2xl" />
+							<span className="relative z-20">Projects</span>
+							<div className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r  from-transparent via-indigo-500 to-transparent shadow-2xl" />
+						</Link>
+						<Link
+							href="/contact"
+							className="relative rounded-full border border-slate-800 bg-slate-900 px-8 py-2 text-sm text-white transition duration-300 hover:border-slate-700 hover:bg-slate-800">
+							<div className="absolute inset-x-0 -top-px mx-auto h-px w-1/2 bg-gradient-to-r  from-transparent via-indigo-500 to-transparent shadow-2xl" />
+							<span className="relative z-20">Contact</span>
+							<div className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r  from-transparent via-indigo-500 to-transparent shadow-2xl" />
+						</Link>
 					</div>
 				</Vortex>
 			</div>
