832015766ddd80f8e608efc8397a34a2026d7619
matthewglasser <matthewsglasser@gmail.com>
2024-06-02 21:33:52 -0400

    email done

--- app/api/sendEmailToMe/route.tsx
+++ app/api/sendEmailToMe/route.tsx
@@ -1,14 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
+import { z } from "zod";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export function POST(request: NextRequest) {
+export async function POST(request: NextRequest) {
+	const requestJSON = await request.json();
+
+	const { email, subject, message } = z
+		.object({ email: z.string().email(), subject: z.string(), message: z.string() })
+		.parse(requestJSON);
+
 	resend.emails.send({
 		from: "onboarding@resend.dev",
 		to: "matthewsglasser@gmail.com",
-		subject: "Hello World",
-		html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
+		subject: subject,
+		html: `<p>${email}\n\n${message}</p>`,
 	});
 
 	return NextResponse.json({});
