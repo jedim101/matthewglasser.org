cb862e6e10fa25d806495f3ca705ad2713004ebb
matthewglasser <matthewsglasser@gmail.com>
2024-06-04 10:13:35 -0400

    better footer for mobile

--- components/footer.tsx
+++ components/footer.tsx
@@ -15,16 +15,17 @@ const socials = [
 
 export default function Footer() {
 	return (
-		<div className="text-slate font-sm flex w-full flex-col items-center border-t border-zinc-700 bg-zinc-900 p-10 text-sm font-light text-gray-400">
-			<div className="mb-4 flex w-1/3 justify-center space-x-8 border-b border-gray-600 px-10 pb-4 text-base">
+		<div className="text-slate font-sm flex w-full flex-col items-center space-y-4 border-t border-zinc-700 bg-zinc-900 p-10 text-sm font-light text-gray-400">
+			<div className="flex justify-center space-x-8 text-base">
 				{links.map((link) => (
 					<Link href={link.href} key={link.display} className="hover:text-gray-200">
 						{link.display}
 					</Link>
 				))}
 			</div>
-			&copy; 2024 Matthew Glasser. All rights reserved.
-			<div className="flex justify-center space-x-4 pt-4">
+			<div className="h-px w-96 bg-gradient-to-r from-transparent via-gray-600" />
+			<p>&copy; 2024 Matthew Glasser. All rights reserved.</p>
+			<div className="flex justify-center space-x-4">
 				{socials.map((social) => (
 					<Link href={social.href} key={social.href} target="_blank" className="h-6 w-6 hover:text-gray-200">
 						{social.icon}
