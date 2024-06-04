684c55821020a3bd9925cdd379b81771b61d643d
matthewglasser <matthewsglasser@gmail.com>
2024-06-03 20:47:38 -0400

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Matthew Glasser",
-	description: "",
+	description: "Full Stack Software Engineer & Web Developer",
 };
 
 export default function RootLayout({
