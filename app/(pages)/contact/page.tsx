bc9034fc0364d9975db7075452e5bf6de847b575
matthewglasser <matthewsglasser@gmail.com>
2024-09-30 15:38:02 -0400

    add cal.com

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -1,90 +1,56 @@
 "use client";
 
 import TitleReveal from "@/components/titleReveal";
-import { sendEmails } from "@/lib/serverActions";
+import { getCalApi } from "@calcom/embed-react";
+import { CalendarIcon } from "@heroicons/react/20/solid";
+import { CalendarDaysIcon, EnvelopeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect } from "react";
 
-export default function About() {
-	const [email, setEmail] = useState("");
-	const [subject, setSubject] = useState("");
-	const [message, setMessage] = useState("");
+export default function Contact() {
+	useEffect(() => {
+		(async function () {
+			const cal = await getCalApi({ namespace: "30min" });
+			cal("ui", {
+				theme: "dark",
+				styles: { branding: { brandColor: "#000000" } },
+				hideEventTypeDetails: false,
+				layout: "month_view",
+			});
+			cal("preload", { calLink: "matthewglasser/30min" });
+		})();
+	}, []);
 
 	return (
 		<main className="flex min-h-screen w-screen flex-col items-center space-y-16 bg-gray-900 p-12 pt-24">
 			<TitleReveal className="h-16 text-6xl font-semibold text-white" text="Contact" />
+			<div className="relative flex w-full max-w-3xl gap-16">
+				<div className="relative h-64 w-full">
+					<div className="absolute -inset-2 bg-gradient-to-r from-purple-500 to-pink-500 opacity-20 blur-xl" />
 
-			<p className="max-w-4xl text-center text-lg text-gray-300">
-				You can send me a message here, or email me at{" "}
-				<Link
-					href="mailto:matthew@matthewglasser.org"
-					target="_blank"
-					className="duration-150hover:text-blue-600 font-semibold text-blue-500">
-					matthew@matthewglasser.org
+					<button
+						className="group relative z-50 flex h-full w-full flex-col items-center justify-center space-y-24 rounded-xl border border-slate-700 bg-gray-900 p-12 duration-300"
+						data-cal-namespace="30min"
+						data-cal-link="matthewglasser/30min"
+						data-cal-config='{"layout":"month_view","theme":"dark"}'>
+						<div className="flex flex-col items-center gap-2">
+							<CalendarDaysIcon className="size-16 text-indigo-700" />
+							<span className="text-xl font-bold">Book a Meeting</span>
+						</div>
+					</button>
+				</div>
+
+				<Link href="/email" className="relative w-full">
+					<div className="absolute -inset-2 bg-gradient-to-r from-purple-500 to-pink-500 opacity-20 blur-xl" />
+
+					<div className="relative z-50 flex h-full w-full flex-col items-center justify-center space-y-24 rounded-xl border border-slate-700 bg-gray-900 p-12">
+						<div className="flex flex-col items-center gap-2">
+							<EnvelopeIcon className="size-16 text-indigo-700" />
+							<span className="text-xl font-bold">Email Me</span>
+						</div>
+					</div>
 				</Link>
-				.
-			</p>
-
-			<form
-				onSubmit={async (e) => {
-					e.preventDefault;
-
-					// await fetch("/api/sendEmailToMe", {
-					// 	method: "POST",
-					// 	body: JSON.stringify({
-					// 		email,
-					// 		subject,
-					// 		message,
-					// 	}),
-					// });
-					sendEmails(email, subject, message);
-				}}
-				className="w-screen space-y-6 border-y border-white/10 bg-white/5 px-12 py-10 sm:max-w-xl sm:rounded-xl sm:border-x">
-				<div className="space-y-2">
-					<label htmlFor="email">Email</label>
-					<input
-						id="email"
-						type="email"
-						placeholder="matthew@matthewglasser.org"
-						className="flex w-full rounded-md border border-white/10 bg-white/5 px-3 py-2 text-sm ring-indigo-500 ring-offset-transparent focus-visible:outline-none focus-visible:ring-1"
-						required
-						onChange={(e) => setEmail(e.target.value)}
-						value={email}
-					/>
-				</div>
-				<div className="space-y-2">
-					<label htmlFor="subject">Subject</label>
-					<input
-						id="subject"
-						type="text"
-						placeholder="Let's Chat!"
-						className="flex w-full rounded-md border border-white/10 bg-white/5 px-3 py-2 text-sm ring-indigo-500 ring-offset-transparent focus-visible:outline-none focus-visible:ring-1"
-						required
-						onChange={(e) => setSubject(e.target.value)}
-						value={subject}
-					/>
-				</div>
-				<div className="space-y-2">
-					<label htmlFor="message">Message</label>
-					<textarea
-						id="message"
-						placeholder="Your message here..."
-						rows={8}
-						className="flex w-full rounded-md border border-white/10 bg-white/5 px-3 py-2 text-sm ring-indigo-500 ring-offset-transparent focus-visible:outline-none focus-visible:ring-1"
-						required
-						onChange={(e) => setMessage(e.target.value)}
-						value={message}
-					/>
-				</div>
-				<div className="h-px w-full bg-gradient-to-r from-transparent via-gray-500" />
-				<button className="group relative block h-10 w-full rounded-md border border-white/10 bg-white/5" type="submit">
-					Send &rarr;
-					<>
-						<span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover:opacity-100" />
-						<span className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover:opacity-100" />
-					</>
-				</button>
-			</form>
+			</div>
 		</main>
 	);
 }
