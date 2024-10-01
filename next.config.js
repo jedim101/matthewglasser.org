636d384a8e0ea38b0cacdc0f3b1358e424b7b833
matthewglasser <matthewsglasser@gmail.com>
2024-09-30 20:52:07 -0400

    schedule redirects

--- next.config.js
+++ next.config.js
@@ -1,1 +1,16 @@
-module.exports = {};
+module.exports = {
+	async redirects() {
+		return [
+			{
+				source: "/schedule/15min",
+				destination: "https://cal.com/matthewglasser/15min",
+				permanent: true,
+			},
+			{
+				source: "/schedule/30min",
+				destination: "https://cal.com/matthewglasser/30min",
+				permanent: true,
+			},
+		];
+	},
+};
